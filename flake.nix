93b4855dba93d5a93fe0e45660616b040cddce79
waalge <waagle@jes>
2024-09-04 08:17:40 +0000

    fails

--- flake.nix
+++ flake.nix
@@ -6,7 +6,8 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     pre-commit-hooks-nix.url = "github:hercules-ci/pre-commit-hooks.nix/flakeModule";
     pre-commit-hooks-nix.inputs.nixpkgs.follows = "nixpkgs";
-    aiken.url = "github:aiken-lang/aiken/v1.0.29-alpha";
+    # aiken.url = "github:aiken-lang/aiken/v1.0.29-alpha";
+    aiken.url = "github:waalge/aiken/waalge/bump-flake";
     treefmt-nix.url = "github:numtide/treefmt-nix";
   };
 
